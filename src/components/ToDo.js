0f510e6cde3f5045a98c6216b63e316de4f36d58
Mario González <mario@mariogl.com>
2021-05-13 19:57:53 +0200

    Colors by priority

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,20 +3,34 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  makeStyles,
 } from "@material-ui/core";
 import DoneIcon from "@material-ui/icons/Done";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { useHistory } from "react-router";
 import { routePaths } from "../router/paths";
 
+const useStyles = makeStyles({
+  priority1: {
+    backgroundColor: "red",
+  },
+  priority2: {
+    backgroundColor: "yellow",
+  },
+  priority3: {
+    backgroundColor: "green",
+  },
+});
+
 export const ToDo = (props) => {
   const {
-    toDo: { id, description },
+    toDo: { id, description, priority },
   } = props;
+  const classes = useStyles();
   const history = useHistory();
   const goToEditPage = () => history.push(`${routePaths.editToDo}/${id}`);
   return (
-    <ListItem button>
+    <ListItem className={classes[`priority${priority}`]} button>
       <ListItemAvatar>
         <Avatar>
           <DoneIcon />

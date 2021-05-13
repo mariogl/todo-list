192e8a13b0ced855a07a69cdfdf68c4d44198849
Mario González <mario@mariogl.com>
2021-05-13 19:16:31 +0200

    Added bin icon

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,19 +5,25 @@ import {
   ListItemText,
 } from "@material-ui/core";
 import DoneIcon from "@material-ui/icons/Done";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 export const ToDo = (props) => {
   const {
     toDo: { description },
   } = props;
   return (
-    <ListItem disableGutters>
+    <ListItem button disableGutters>
       <ListItemAvatar>
         <Avatar>
           <DoneIcon />
         </Avatar>
       </ListItemAvatar>
       <ListItemText>{description}</ListItemText>
+      <ListItemAvatar>
+        <Avatar>
+          <DeleteIcon />
+        </Avatar>
+      </ListItemAvatar>
     </ListItem>
   );
 };
